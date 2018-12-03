2a02abef3db0d760e487af51bba4a74fd3a0d155
Carl Smith <carlfsmithiii@gmail.com>
2018-12-03 11:11:30 -0500

    basic shell of main page, with mock components

---
+++ src/Components/UserList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const styles = {
+  UserList: {
+    margin: "auto",
+    background: "#38a445",
+    color: "white",
+    textAlign: "center",
+    // width: "40vw",
+    height: "70vh"
+  }
+};
+
+const UserList = props => <div style={styles.UserList}>The UserList</div>;
+
+export default UserList;
