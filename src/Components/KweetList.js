9b066fbbaf7a5eda2b3f6feefe1b702d3077abd2
jayaimzzz <jameshorton@gmail.com>
2018-12-04 17:00:04 -0500

    feed is populating with kweets

--- src/Components/KweetList.js
+++ src/Components/KweetList.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import Kweet from "./Kweet"
 
 const styles = {
   KweetList: {
@@ -6,12 +8,36 @@ const styles = {
     marginTop: 10,
     background: "#493154",
     color: "white",
-    textAlign: "center",
-    width: "40vw",
-    height: "60vh"
+    textAlign: "center"
+    // width: "40vw",
+    // height: "60vh"
   }
 };
 
-const KweetList = props => <div style={styles.KweetList}>The KweetList</div>;
+// const KweetList = props => <div style={styles.KweetList}>The KweetList</div>;
+class KweetList extends Component {
+  render() {
+    console.log(this.props.messages.messages);
+    return (
+      <Fragment>
+        <h1 style={styles.KweetList}>Kweet Feed</h1>
+        {this.props.messages.messages.map(message => (
+          <Kweet key={message.id}
+            text={message.text}
+            userId={message.userId}
+          />
+        ))}
+      </Fragment>
+    );
+  }
+}
 
-export default KweetList;
+const mapStateToProps = state => {
+  return { messages: state.messages };
+};
+const mapDispatchToProps = null;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(KweetList);
