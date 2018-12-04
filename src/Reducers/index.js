75f058160402f1031f284332c90c012be593bc9b
Carl Smith <carlfsmithiii@gmail.com>
2018-12-03 22:22:49 -0500

    combined LoggedInUserReducer, MessagesReducer, and UsersReducer

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,5 +1,12 @@
 import { combineReducers } from "redux";
+import LoggedInUserReducer from './LoggedInUserReducer';
+import MessagesReducer from './MessagesReducer';
+import UsersReducer from './UsersReducer';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+    loggedInUser: LoggedInUserReducer,
+    messages: MessagesReducer,
+    users: UsersReducer
+});
 
 export default rootReducer;
