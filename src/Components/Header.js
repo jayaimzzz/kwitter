c491a81fa8e83c0dfd2c18378bbcc0097a931c4f
Austin Levine <austinlevine4@gmail.com>
2018-12-04 19:10:00 -0500

    added base for header w/ logo and a nav bar with buttons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Logo from "../logo.png";
+import Typography from "@material-ui/core/Typography";
 
 const styles = {
   Header: {
@@ -7,11 +9,19 @@ const styles = {
     background: "#033c99",
     color: "white",
     textAlign: "center",
+    fontSize: "50px",
     width: "60vw",
     height: "15vh"
   }
 };
 
-const Header = props => <div style={styles.Header}>The Header</div>;
+// const Header = props => <div style={styles.Header}>The Header</div>;
 
+const Header = props => {
+  return (
+    <div style={styles.Header}>
+      <img src={Logo} /> Switter
+    </div>
+  );
+};
 export default Header;
