d73f78e23eacb42772669693e294ec10834f03fc
jayaimzzz <jameshorton@gmail.com>
2018-12-07 14:45:09 -0500

    finished styling kweet.

--- src/Components/Kweet.js
+++ src/Components/Kweet.js
@@ -3,34 +3,21 @@ import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-// import DeleteIcon from '@material-ui/icons/Delete';
-// import PhotoCamera from '@material-ui/icons/PhotoCamera'
-import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
 import ThumbUp from "@material-ui/icons/ThumbUp";
 import Delete from "@material-ui/icons/Delete";
 import Avatar from "@material-ui/core/Avatar";
-import Grid from "@material-ui/core/Grid";
-import { FormHelperText } from "@material-ui/core";
 import CardHeader from "@material-ui/core/CardHeader";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import moment from 'moment'
+import moment from "moment";
 
 const styles = {
   card: {
     minWidth: 275
   },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
-  },
   kweet: {
     fontSize: 18,
-    color: "black",
-    marginLeft: 10
+    color: "black"
   },
   pos: {
     marginTop: 12
@@ -44,24 +31,15 @@ const styles = {
   },
   date: {
     fontSize: 16
+  },
+  separator: {
+    flexGrow: 2
   }
 };
 
 class Kweet extends Component {
   render() {
     const { classes } = this.props;
-    const unixTime = Date.parse(this.props.createdAt);
-    const dateOfKweet = new Date(unixTime).getTimezoneOffset()
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    const dayOfWeek = daysOfWeek[dateOfKweet.getDay()]
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    const month = months[dateOfKweet.getMonth()]
-    const hour = this.props.createdAt.slice(11,13)
-    const minutes = this.props.createdAt.slice(13,16)
-    const time = `${hour}${minutes}`
-    console.log(this.props.createdAt)
-    console.log(minutes)
-    const formattedDate = `${time} UTC ${dayOfWeek}, ${month} ${dateOfKweet.getDate()}, ${dateOfKweet.getFullYear()}`
     let userPhotoSrc =
       "http://www.dealnetcapital.com/wp-content/blogs.dir/9/files/2014/10/blank-profile.png";
 
@@ -69,20 +47,31 @@ class Kweet extends Component {
       <Card className={classes.pos}>
         <CardHeader
           avatar={<Avatar src={userPhotoSrc} className={classes.avatar} />}
-          action={formattedDate}
+          action={<Typography>{moment(this.props.createdAt).fromNow()}</Typography>}
           title={this.props.username + ":"}
           classes={{
             title: classes.title,
             action: classes.date
           }}
         />
-        <Typography className={classes.kweet}>"{this.props.text}"</Typography>
+        <CardContent>
+          <Typography className={classes.kweet}>"{this.props.text}"</Typography>
+        </CardContent>
         <CardActions>
-          <IconButton>
+          <IconButton
+            onClick={() => console.log("Like kweet button clicked")}
+            className={classes.like}
+          >
             <ThumbUp />
           </IconButton>
-          <Typography>{this.props.likes.length}</Typography>
-          <IconButton>
+          <Typography className={classes.like}>
+            {this.props.likes.length}
+          </Typography>
+          <Typography className={classes.separator} />
+          <IconButton
+            onClick={() => console.log("Delete kweet button clicked")}
+            className={classes.delete}
+          >
             <Delete />
           </IconButton>
         </CardActions>
