1c8554b5ab5a5ff31b42204e03917c586c20e84e
Carl Smith <carlfsmithiii@gmail.com>
2018-12-14 10:20:28 -0500

    Updates

---
+++ precache-manifest.394f1387d967e2bb9973431a30dd6466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "09a7380da0abd69fc53f",
+    "url": "/carlfsmithiii/kwitter/static/js/main.09a7380d.chunk.js"
+  },
+  {
+    "revision": "9ca468e28bed08c42544",
+    "url": "/carlfsmithiii/kwitter/static/js/1.9ca468e2.chunk.js"
+  },
+  {
+    "revision": "f2af29c5db3182167d7d",
+    "url": "/carlfsmithiii/kwitter/static/js/runtime~main.f2af29c5.js"
+  },
+  {
+    "revision": "5f64a52267bac4635182cd4728a4c10d",
+    "url": "/carlfsmithiii/kwitter/static/media/kweet2.5f64a522.jpg"
+  },
+  {
+    "revision": "55890f9a30fcb913543d33d243ea15e2",
+    "url": "/carlfsmithiii/kwitter/static/media/Kweet.55890f9a.png"
+  },
+  {
+    "revision": "687351cf3f5a162d5876130dcb4171e1",
+    "url": "/carlfsmithiii/kwitter/index.html"
+  }
+];
